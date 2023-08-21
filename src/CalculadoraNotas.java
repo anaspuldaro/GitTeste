dfa7338fd0d4c8d7bd5e2997bc7c6294d4e6cffe
Ana Caroline <spuldaroanacaroline16@gmail.com>
2023-08-21 14:43:02 -0300

    Calcular Media

--- src/CalculadoraNotas.java
+++ src/CalculadoraNotas.java
@@ -1,9 +1,30 @@
 
+import java.util.Scanner;
 public class CalculadoraNotas {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner scanner = new Scanner(System.in);
+		double[] notas = new double[5];
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.print("Digite a nota do aluno:" + (i+1) +":");
+			notas[i] = scanner.nextDouble();
+			}
+		double soma = calcularSoma(notas);
+		System.out.println("A soma das notas é:" + soma);
+		
+		scanner.close();
 	}
-
+	public static double calcularSoma(double[] notas) {
+		double soma = 0;
+		for (double nota : notas) {
+			soma += nota;
+		}
+		return soma;
+	}
+	public static double calcularMedia(double[] notas) {
+		double soma = calcularSoma(notas);
+		return soma /notas.length;
+	}
 }
