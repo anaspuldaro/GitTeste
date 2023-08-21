de17e4a1e976ca645908cdfa5caf0c5e7f068695
Ana Caroline <spuldaroanacaroline16@gmail.com>
2023-08-21 15:46:44 -0300

    Menor e maior nota

--- src/CalculadoraNotas.java
+++ src/CalculadoraNotas.java
@@ -8,16 +8,22 @@ public class CalculadoraNotas {
 		double[] notas = new double[5];
 		
 		for (int i = 0; i < 5; i++) {
-			System.out.print("Digite a nota do aluno:" + (i+1) +":");
+			System.out.print("Digite a nota do aluno: " + (i+1) +" : ");
 			notas[i] = scanner.nextDouble();
 			
 			}
 		double soma = calcularSoma(notas);
-		System.out.println("A soma das notas é:" + soma);
+		System.out.println("A soma das notas é: " + soma);
 		double media = calcularMedia(notas);
-		System.out.println("A média das notas é:" +media);
+		System.out.println("A média das notas é: " +media);
+		double menornota = menorNota(notas);
+		System.out.println("A menor nota é: " +menornota);
+		double maiornota = maiorNota(notas);
+		System.out.println("A maior nota é: " +maiornota);
 		
-		System.out.println("As notas menores de que seis são:");
+		
+		
+		System.out.println("As notas menores de que seis são: ");
 		notasMenoresQueSeis(notas);
 		
 		scanner.close();
@@ -39,5 +45,24 @@ public class CalculadoraNotas {
 				System.out.println(nota);
 			}
 		}
+	}
+	public static double menorNota(double[] notas) {
+		double menor = notas[0];
+		for (double nota : notas) {
+			if (nota < menor) {
+				menor = nota;
+			}
+		}
+		return menor;
+		
+	}
+	public static double maiorNota(double[] notas) {
+		double maior = notas[0];
+		for (double nota : notas) {
+			if (nota > maior) {
+				maior = nota;
+			}
+		}
+		return maior;
 	}
 }
