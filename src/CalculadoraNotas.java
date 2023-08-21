85d9b18acd1269d0c3fe923bb5bcf6b9040160d4
Ana Caroline <spuldaroanacaroline16@gmail.com>
2023-08-21 14:50:59 -0300

    Notas menores que 6

--- src/CalculadoraNotas.java
+++ src/CalculadoraNotas.java
@@ -10,9 +10,15 @@ public class CalculadoraNotas {
 		for (int i = 0; i < 5; i++) {
 			System.out.print("Digite a nota do aluno:" + (i+1) +":");
 			notas[i] = scanner.nextDouble();
+			
 			}
 		double soma = calcularSoma(notas);
 		System.out.println("A soma das notas é:" + soma);
+		double media = calcularMedia(notas);
+		System.out.println("A média das notas é:" +media);
+		
+		System.out.println("As notas menores de que seis são:");
+		notasMenoresQueSeis(notas);
 		
 		scanner.close();
 	}
@@ -26,5 +32,12 @@ public class CalculadoraNotas {
 	public static double calcularMedia(double[] notas) {
 		double soma = calcularSoma(notas);
 		return soma /notas.length;
+	}
+	public static void notasMenoresQueSeis(double[] notas) {
+		for (double nota : notas) {
+			if (nota < 6) {
+				System.out.println(nota);
+			}
+		}
 	}
 }
